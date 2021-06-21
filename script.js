270f758eb7cff10f96ab1388fad78a1a95c60231
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-06-21 02:52:05 +0100

    confuse with querie

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const carbtn = document.getElementById('show-cr');
 const carlist = document.getElementById('list-cr');
 const navbtn = document.getElementById('menu-btn');
 const navlist = document.getElementById('top-list');
-const conta = document.getElementById('main-dv');
+const contadv = document.getElementById('main-dv');
 
 
 function tooglecarl(){ carlist.classList.toggle('visible')};
 function tooglenav(){ navlist.classList.toggle('visible');
-conta.classlist.toggle('shrink')};
+contadv.classList.toggle('shrink')};
 
 carbtn.addEventListener('click',tooglecarl);
 navbtn.addEventListener('click',tooglenav);
